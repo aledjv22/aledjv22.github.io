94c5f1852e82206fe5e54c6040a6787466ab04c8
Alejandro Díaz <victor.diaz.jauregui@mi.unc.edu.ar>
2023-10-25 23:20:30 -0300

    mobile navbar removed

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -2,7 +2,7 @@ import scroll from "../../Scripts/scroll";
 
 const Navbar = () => {
   return (
-    <nav className='bg-[#202020] shadow-lg shadow-[#202020]/50 h-[40px] mr-2 ml-2 font-navbar'>
+    <nav className='bg-[#202020] shadow-lg shadow-[#202020]/50 h-[40px] mr-2 ml-2 font-navbar max-sm:hidden'>
       <ul className='flex justify-evenly text-[18px] pt-2 text-white underline-offset-4'>
         <li>
           <a className='hover:underline' id='about-me' onClick={() => scroll('about-me')}>
